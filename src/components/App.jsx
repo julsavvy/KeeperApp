79a3d3c47635ab366c0305c6da4f4a052bb4c3a9
julsavvy <60718601+julsavvy@users.noreply.github.com>
2020-09-02 09:39:28 -0400

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+function App()jqch {
+  return (
+    <div>
+      <Header />
+      <Note />hjascj
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
